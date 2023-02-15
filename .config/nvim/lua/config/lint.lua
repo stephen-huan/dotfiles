690896496a5fffe2a65bbadacf6f9af7291ef864
Stephen Huan <stephen03.huan@gmail.com>
2023-02-15 00:59:52 -0500

    refactor: re-use package specification in lsp.lua

--- .config/nvim/lua/config/lint.lua
+++ .config/nvim/lua/config/lint.lua
@@ -1,17 +1,30 @@
 local lint = require("lint")
+local lsp = require("config.lsp")
 
 -- register linters for each filetype
-lint.linters_by_ft = {
-    lua = { "selene" },
-}
+lint.linters_by_ft = {}
+local languages = {}
+for language, types in pairs(lsp.packages) do
+    if types.linter then
+        lint.linters_by_ft[language] = types.linter
+        table.insert(languages, language)
+    end
+end
 -- autocmd
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+vim.api.nvim_create_autocmd({ "FileType" }, {
     group = "vimrc",
-    pattern = {
-        "*.lua",
-    },
-    callback = function()
-        lint.try_lint()
+    pattern = languages,
+    callback = function(args)
+        local events = { "BufEnter", "BufWritePost", "InsertLeave" }
+        vim.api.nvim_create_autocmd(events, {
+            group = vim.api.nvim_create_augroup(
+                string.format("lint<buffer=%d>", args.buf), { clear = true }
+            ),
+            buffer = args.buf,
+            callback = function()
+                lint.try_lint()
+            end,
+        })
     end,
 })
 
--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,17 +1,33 @@
 -- installed mason packages
 local packages = {
-    -- lua
-
-    -- lsp: https://github.com/LuaLS/lua-language-server
-    "lua-language-server",
-    -- linter: https://github.com/Kampfkarren/selene
-    "selene",
+    lua = {
+        lsp = {
+            -- https://github.com/LuaLS/lua-language-server
+            "lua-language-server",
+        },
+        linter = {
+            -- https://github.com/Kampfkarren/selene
+            "selene",
+        },
+    },
+    test = {
+        lsp = {
+            "hi"
+        }
+    }
 }
 
 -- command to install specified packages
 vim.api.nvim_create_user_command("MasonInstallAll", function()
-    for _, package in ipairs(packages) do
-        vim.cmd("MasonInstall " .. package)
+    for _, language in pairs(packages) do
+        for type, list in pairs(language) do
+            for _, package in pairs(list) do
+                vim.cmd("MasonInstall " .. package)
+            end
+        end
     end
 end, {})
 
+return {
+    packages = packages,
+}
